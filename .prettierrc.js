0e9c76dbd9e8a991b7a928d2fb91af86e99fea04
Gianluca Acerbis <acerbisgianluca@gmail.com>
2022-02-06 17:22:53 +0100

    Add library

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'consistent',
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'always',
+};
